25f45b8d552313adaba3708c96368cf8b3bb1fb1
Łukasz Włódarczyk <lukasz.createam@gmail.com>
2019-11-16 00:51:26 +0100

    added support for x64 systems

--- src/main/java/io/github/kilmajster/ngrok/exception/NgrokDownloadException.java
+++ src/main/java/io/github/kilmajster/ngrok/exception/NgrokDownloadException.java
@@ -4,4 +4,8 @@ public class NgrokDownloadException extends RuntimeException {
     public NgrokDownloadException(Throwable throwable) {
         super(throwable);
     }
+
+    public NgrokDownloadException(String message) {
+        super(message);
+    }
 }
