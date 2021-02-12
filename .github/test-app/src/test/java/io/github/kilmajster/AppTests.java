91f7e605a7c9940035007403a26487271095d783
kilmajster <lukasz.createam@gmail.com>
2021-02-13 00:28:25 +0100

    tests polish

--- .github/test-app/src/test/java/io/github/kilmajster/AppTests.java
+++ .github/test-app/src/test/java/io/github/kilmajster/AppTests.java
@@ -1,10 +1,6 @@
 package io.github.kilmajster;
 
-import org.apache.commons.io.IOUtils;
 import org.apache.commons.lang3.StringUtils;
-
-import org.junit.jupiter.api.AfterAll;
-import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.slf4j.Logger;
@@ -12,7 +8,6 @@ import org.slf4j.LoggerFactory;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.system.CapturedOutput;
 import org.springframework.boot.test.system.OutputCaptureExtension;
-import org.springframework.boot.test.system.OutputCaptureRule;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.client.RestTemplate;
@@ -20,20 +15,19 @@ import org.springframework.web.client.RestTemplate;
 import java.io.IOException;
 import java.net.URISyntaxException;
 import java.net.URL;
-import java.nio.charset.StandardCharsets;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
 @SpringBootTest(
 		webEnvironment = SpringBootTest.WebEnvironment.DEFINED_PORT,
-		properties = "ngrok.enabled=true"
+		properties = {
+				"ngrok.enabled=true"
+		}
 )
 @ExtendWith(OutputCaptureExtension.class)
 class AppTests {
 
 	private final static Logger log = LoggerFactory.getLogger(AppTests.class);
-
-
 	private static final int WAIT_FOR_STARTUP_SECONDS = 30;
 
 	@Test
@@ -44,7 +38,6 @@ class AppTests {
 
 		for (int i = WAIT_FOR_STARTUP_SECONDS; i > 0; i--) {
 			Thread.sleep(1000);
-
 			if(output.toString().contains("(http) ->")) {
 				ngrokStarted = true;
 				log.info("[TEST] Ngrok start detected!");
@@ -55,10 +48,10 @@ class AppTests {
 		assertThat(ngrokStarted).isTrue();
 
 		final String ngrokHttpsRemoteUrl = StringUtils.substringBetween(output.toString(), "(https) ->", "\n");
-
 		log.info("[TEST] Ngrok tunnel url = [{}]", ngrokHttpsRemoteUrl);
 
 		ResponseEntity<String> responseFromTunnel = new RestTemplate().getForEntity(new URL(ngrokHttpsRemoteUrl).toURI(), String.class);
+		log.info("Response from [{}] = \n\n{}\n\n", ngrokHttpsRemoteUrl, responseFromTunnel.toString());
 
 		assertThat(responseFromTunnel.getStatusCode()).isEqualTo(HttpStatus.OK);
 		assertThat(responseFromTunnel.getBody()).isEqualTo("<h1>Hello World!</h1>");
