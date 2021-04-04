921243c49d937228128079fecc17acbe82251f0e
kilmajster <lukasz.createam@gmail.com>
2021-04-04 21:21:05 +0200

    mac os pipeline fix

--- .github/scripts/set-current-starter-version-macos.sh
+++ .github/scripts/set-current-starter-version-macos.sh
@@ -6,4 +6,4 @@ echo "Current ngrok-spring-boot-starter version is $CURRENT_STARTER_VERSION"
 sed -i -e "s/ci-version-placeholder/$CURRENT_STARTER_VERSION/g" ../test-app/pom.xml
 
 TEST_APP_STARTER_DEPENDENCY_VERSION=$(xmllint --xpath "//*[local-name()='project']/*[local-name()='properties']/*[local-name()='ngrok-spring-boot-starter.version']/text()" ../test-app/pom.xml)
-echo "Starter dependency version successfully updated to $TEST_APP_STAbRTER_DEPENDENCY_VERSION"
+echo "Starter dependency version successfully updated to $TEST_APP_STARTER_DEPENDENCY_VERSION"
